99d4b70abe2bda1b98180ff5f9cfd528e471032e
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-09-29 22:15:42 +0530

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n>0){
+        cout<<"Positive number";
+
+    }
+    else{
+        cout<<"Negative Number";
+    }
+}
